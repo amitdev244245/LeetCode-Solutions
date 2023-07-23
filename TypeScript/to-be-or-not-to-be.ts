05baa482b66f652833fe4fe42c897c360a212d0e
Amit Sharma <amitdev244245@gmail.com>
2023-07-24 02:10:24 +0530

    added To Be Or Not To Be typescript program

---
+++ TypeScript/to-be-or-not-to-be.ts
@@ -0,0 +1,26 @@
+// Problem No: 2704
+// Problem Name: To Be Or Not To Be
+// Time:  O(1)
+// Space: O(1)
+
+type ToBeOrNotToBe = {
+    toBe: (val: any) => boolean;
+    notToBe: (val: any) => boolean;
+};
+
+function expect(val: any): ToBeOrNotToBe {
+    return {
+        toBe: (val2: any): boolean => {
+            if (val !== val2) {
+                throw new Error("Not Equal");
+            }
+            return true;            
+        },
+        notToBe: (val2: any): boolean => {
+            if (val === val2) {
+                throw new Error("Equal");
+            }
+            return true;            
+        }
+    };
+};
