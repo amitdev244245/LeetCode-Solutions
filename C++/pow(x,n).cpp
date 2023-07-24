b42eda96cec9d893be96a2d3872ee7ade8bcb026
Amit Sharma <amitdev244245@gmail.com>
2023-07-25 05:23:21 +0530

    added program #50 Pow(x,n) in C++

---
+++ C++/pow(x,n).cpp
@@ -0,0 +1,17 @@
+// Problem No: 50
+// Problem Name: Pow(x, n) (Medium)
+// Time:  O(1)
+// Space: O(1)
+
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+
+class Solution
+{
+public:
+    double myPow(double x, int n)
+    {
+        return pow(x, n);
+    }
+};
