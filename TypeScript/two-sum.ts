25511669938a59d0b13e15da7b45cce017289f71
Amit Sharma <amitdev244245@gmail.com>
2023-07-27 02:58:22 +0530

    added program #1 in TypeScript

---
+++ TypeScript/two-sum.ts
@@ -0,0 +1,12 @@
+// Problem No: 1
+// Problem Name: Two Sum
+
+function twoSum(nums: number[], target: number): number[] {
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] == target) {
+        return [i, j];
+      }
+    }
+  }
+}
