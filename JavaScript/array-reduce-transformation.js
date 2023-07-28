8df201cbf16cb3a2aed7e101c3af27a0f218c0ed
Amit Sharma <amitdev244245@gmail.com>
2023-07-28 05:44:55 +0530

    added program #2626 array-reduce-transformation in JavaScript

---
+++ JavaScript/array-reduce-transformation.js
@@ -0,0 +1,12 @@
+// Problem No: 2626
+// Problem Name: Array Reduce Transformation
+// Time:  O(n)
+// Space: O(1)
+
+var reduce = function (nums, fn, init) {
+    let result = init;
+    for (let i = 0; i < nums.length; i++) {
+        result = fn(result, nums[i]);
+    }
+    return result;
+};
