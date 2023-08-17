f02f8831a9b474c8548af67da7e600c3e01a0ab9
Amit Sharma <amitdev244245@gmail.com>
2023-08-17 06:45:55 +0530

    Updated program link #2620 counter in TypeScript

--- TypeScript/counter.ts
+++ TypeScript/counter.ts
@@ -1,10 +1,11 @@
 // Problem No: 2620
 // Problem Name: Counter
+// Problem Link: https://leetcode.com/problems/counter
 // Time:  O(1)
 // Space: O(1)
 
 function createCounter(n: number): () => number {
-    return function() {
-        return n++;
-    }
+  return function () {
+    return n++;
+  };
 }
