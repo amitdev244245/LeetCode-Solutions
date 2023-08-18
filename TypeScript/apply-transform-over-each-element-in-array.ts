81bae8a14a83c98595f9234e3f3cdf0acf298c33
Amit Sharma <amitdev244245@gmail.com>
2023-08-18 23:37:50 +0530

    Updated program link #2635 apply-transform-over-each-element-in-array in TypeScript

--- TypeScript/apply-transform-over-each-element-in-array.ts
+++ TypeScript/apply-transform-over-each-element-in-array.ts
@@ -1,5 +1,6 @@
 // Problem No: 2635
 // Problem Name: Apply Transform Over Each Element in Array
+// Problem Link: https://leetcode.com/problems/apply-transform-over-each-element-in-array
 // Time:  O(n)
 // Space: O(n)
 
