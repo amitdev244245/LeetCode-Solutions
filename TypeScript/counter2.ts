3b80ff9327feb78d54ac792b30c48bc84c9f69e2
Amit Sharma <amitdev244245@gmail.com>
2023-08-18 23:31:45 +0530

    Updated program link #2665 counter2 in TypeScript

--- TypeScript/counter2.ts
+++ TypeScript/counter2.ts
@@ -1,19 +1,20 @@
 // Problem No: 2665
 // Problem Name: Counter II
+// Problem Link: https://leetcode.com/problems/counter-ii
 // Time:  O(1)
 // Space: O(1)
 
 type ReturnObj = {
-    increment: () => number,
-    decrement: () => number,
-    reset: () => number,
-}
+  increment: () => number;
+  decrement: () => number;
+  reset: () => number;
+};
 
 function createCounter(init: number): ReturnObj {
-    let curr = init;
-    return {
-    increment: ():number => ++curr,
-    decrement: ():number => --curr,
-    reset: ():number => (curr=init)
-    }
-};
+  let curr = init;
+  return {
+    increment: (): number => ++curr,
+    decrement: (): number => --curr,
+    reset: (): number => (curr = init),
+  };
+}
