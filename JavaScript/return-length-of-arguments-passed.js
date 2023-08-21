b29cb077c10128b6e540c3d4b65a4ea36c65704d
Amit Sharma <amitdev244245@gmail.com>
2023-08-22 04:58:53 +0530

    Added program #2703 return-length-of-arguments-passed in JavaScript

---
+++ JavaScript/return-length-of-arguments-passed.js
@@ -0,0 +1,7 @@
+// Problem No: 2703
+// Problem Name: Return Length of Arguments Passed
+// Problem Link: https://leetcode.com/problems/return-length-of-arguments-passed
+
+var argumentsLength = function (...args) {
+    return args.length;
+};
